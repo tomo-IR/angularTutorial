80bc7f90b7cb6b86c0f9c7835b6b255a027484d8
tomo <sunnyday20200101@gmail.com>
2022-05-30 22:48:52 +0900

    message追加

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,12 +13,23 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
-  constructor() {}
+  heroes: Hero[] = [];
+  constructor(
+    private heroService: HeroService,
+    private messageServece: MessageService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   selectedHero: Hero | undefined;
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageServece.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+  getHeroes() {
+    this.heroService.getHeroes().subscribe((heroes) => {
+      this.heroes = heroes;
+    });
   }
 }
